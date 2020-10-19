3e2e33dbbde089d938283b9606f395265d185864
Steve Bilogan <steve.bilogan@gmail.com>
2020-10-19 17:25:52 -0400

    update

--- DailyReflection/DailyReflection.Android/Properties/AssemblyInfo.cs
+++ DailyReflection/DailyReflection.Android/Properties/AssemblyInfo.cs
@@ -15,7 +15,11 @@ using Android.App;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-
+#if DEBUG
+[assembly: Application(Debuggable=true)]
+#else
+[assembly: Application(Debuggable = false)]
+#endif
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
