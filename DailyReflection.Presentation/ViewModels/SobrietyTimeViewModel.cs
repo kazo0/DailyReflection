5f91dd772fe214dc1d1116520f42d7c580f2d1c5
Steve Bilogan <steve.bilogan@gmail.com>
2020-12-11 17:57:27 -0500

    Add sober date title to Sobriety Time page

--- DailyReflection.Presentation/ViewModels/SobrietyTimeViewModel.cs
+++ DailyReflection.Presentation/ViewModels/SobrietyTimeViewModel.cs
@@ -11,6 +11,7 @@ namespace DailyReflection.Presentation.ViewModels
 	public class SobrietyTimeViewModel : ViewModelBase, IRecipient<SoberDateChangedMessage>
 	{
 		private Period _soberPeriod;
+		private DateTime? _soberDate;
 		private readonly ISettingsService _settingsService;
 
 		public Period SoberPeriod
@@ -19,22 +20,36 @@ namespace DailyReflection.Presentation.ViewModels
 			set => SetProperty(ref _soberPeriod, value);
 		}
 
+		public DateTime? SoberDate
+		{
+			get => _soberDate;
+			set => SetProperty(ref _soberDate, value);
+		}
+
 		public SobrietyTimeViewModel(ISettingsService settingsService)
 		{
 			_settingsService = settingsService;
+			SoberDate = GetSoberDate();
 			SoberPeriod = GetSoberPeriod();
 		}
 
 		public void Receive(SoberDateChangedMessage message)
 		{
+			SoberDate = GetSoberDate();
 			SoberPeriod = GetSoberPeriod();
 		}
 
 		private Period GetSoberPeriod()
 		{
-			var soberDate = _settingsService.Get(PreferenceConstants.SoberDate, DateTime.Now);
+			var soberDate = SoberDate ?? DateTime.Now;
 			var soberLocalDate = new LocalDate(soberDate.Year, soberDate.Month, soberDate.Day);
 			return new LocalDate(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day) - soberLocalDate;
+		}
+
+		private DateTime? GetSoberDate()
+		{
+			var soberDate = _settingsService.Get(PreferenceConstants.SoberDate, DateTime.MinValue);
+			return soberDate != DateTime.MinValue ? soberDate : null;
 		}
 	}
 }
